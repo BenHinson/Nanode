eda382a43ef34df16861fe3405c1cd8236c4bbdd
BenHinson <48280587+BenHinson@users.noreply.github.com>
2023-05-24 14:10:20 +0100

    Removing react src & public folders

--- src/components/pages/home/header/directoryPath.react.js
+++
@@ -1,7 +0,0 @@
-import React, {useState} from 'react';
-
-export default function DirectoryPath() {
-  return (
-    <div className='directoryLocation' type='text' placeholder='Current Directory'></div> 
-  )
-}
--- src/state/store.react.js
+++
@@ -1,9 +0,0 @@
-import { createStore, applyMiddleware } from "redux";
-import { reducers } from "./reducers.react";
-import thunk from 'redux-thunk';
-
-export const Store = createStore(
-  reducers,
-  {},
-  applyMiddleware(thunk)
-)
